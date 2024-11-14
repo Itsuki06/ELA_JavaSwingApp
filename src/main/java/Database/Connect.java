260bd1c731c4fa04c95711ee007adf9c88582363
Tùng Trịnh <trinhtung0604@gmail.com>
2024-11-14 23:30:20 +0700

    Initial commit

---
+++ src/main/java/Database/Connect.java
@@ -0,0 +1,24 @@
+package Database;
+
+/**
+ * @author TrinhThanhTung
+ */
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class Connect {
+    public static Connection getConnection() throws SQLException {
+        try {
+            String username = "sa";
+            String password = "123456789h";
+            String url = "jdbc:sqlserver://localhost:1433;databasename=ELA_Database;encrypt=true;trustServerCertificate=true;";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            Connection conn = java.sql.DriverManager.getConnection(url, username, password);
+            return conn;
+        } catch (ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "Failed to Connect to Database!", "Announcement", 1);
+        }
+        return null;
+    }    
+}
